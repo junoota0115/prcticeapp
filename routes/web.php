81c231f00bcd42d3ecb7d4b9d68c14c155f478dd
junoota0115 <j1u1n5@gmail.com>
2023-08-17 17:38:55 +0900

    index.blade.phpを表示

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+Route::get('/index', [App\Http\Controllers\PrcticeController::class, 'index'])->name('index');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
