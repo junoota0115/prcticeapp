e553d27d6f224b55ec8b368e495c8fa5d52c3db9
junoota0115 <j1u1n5@gmail.com>
2023-08-17 17:27:15 +0900

    ログイン画面の作成とmigrateファイルの作成

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'prcticeapp'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'junoota0115'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
